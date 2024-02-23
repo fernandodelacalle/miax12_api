32d444f79cc03a5541573c0cfcdc26c17823d74f
Fernando <fernando.decalle@gmail.com>
2024-02-23 19:43:10 +0100

    api draft

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import typing
+
+app = FastAPI()
+
+
+@app.post("/insert_name")
+def insert_name(name: str):
+    pass
+
+
+@app.get("/names")
+def names() -> typing.List[str]:
+    return ["pepe", "maria"]
