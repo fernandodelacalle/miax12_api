d7c65ec9641022135e41db7f4bc30b27c9189fd8
Fernando <fernando.decalle@gmail.com>
2024-02-23 20:22:22 +0100

    no working

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 import typing
+from pymongo import MongoClient
 
 app = FastAPI()
 
 
 @app.post("/insert_name")
 def insert_name(name: str):
+    # conectar al Mongo=mongodb://root:example@mongo:27017
+    mongo_dir = 'mongodb://root:example@mongo:27017'
+    client = MongoClient(mongo_dir)
+    db = client['test_db']
+    col = db['test_coll']
+    col.insert_one({'name':'test2'})
     pass
 
 
