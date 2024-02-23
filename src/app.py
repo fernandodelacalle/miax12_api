a613d38d8a153b92957edb921a83a42b5a3e439e
Fernando <fernando.decalle@gmail.com>
2024-02-23 20:48:48 +0100

    api complete

--- src/app.py
+++ src/app.py
@@ -1,21 +1,31 @@
 from fastapi import FastAPI
 import typing
 from pymongo import MongoClient
+import os
 
 app = FastAPI()
 
 
 @app.post("/insert_name")
 def insert_name(name: str):
-    # conectar al Mongo=mongodb://root:example@mongo:27017
-    mongo_dir = 'mongodb://root:example@mongo:27017'
-    client = MongoClient(mongo_dir)
-    db = client['test_db']
-    col = db['test_coll']
-    col.insert_one({'name':'test2'})
-    pass
+    client = MongoClient(os.getenv("MONGO_ADDR"))
+    db = client['DB_API']
+    col = db['nombres']
+
+    # Check if name already exists in the collection
+    existing_entry = col.find_one({'name': name})
+    if existing_entry:
+        print(f"The name '{name}' already exists in the collection.")
+    else:
+        col.insert_one({'name': name})
+        print(f"The name '{name}' has been inserted into the collection.")
 
 
 @app.get("/names")
 def names() -> typing.List[str]:
-    return ["pepe", "maria"]
+    client = MongoClient(os.getenv("MONGO_ADDR"))
+    db = client['DB_API']
+    col = db['nombres']
+
+    documents = col.find({"name": {"$exists": True}})
+    return [doc['name'] for doc in documents]
